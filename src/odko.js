fd17e07e2c7f72956c5e92d619c2bf2213e9d410
not ervin <notminin@pm.me>
2020-02-28 21:47:22 +0000

    basic ui - update function to update the ui - update function represents columns and rows in dom

--- src/odko.js
+++ src/odko.js
@@ -1,2 +1,57 @@
+// stores references to predefined dom elements
+// and focused elements
+let ui = {
+	fcol: null,   // focused column
+	fblock: null, // focused block
+	flock: false, // lock focus
+
+	content: document.getElementById("content"),
+}
+
+// stores all of the data of the current project
+let project = {
+	columns: [],
+}
+
+// updates dom of content container to reflect the
+// project variable and allow it to be modified.
+function update() {
+	ui.content.innerHTML = "";
+
+	project.columns.length > 0 &&
+		project.columns.forEach((column, x) => {
+			let c = document.createElement("div");
+				c.classList.add("column");
+				ui.content.appendChild(c);
+
+			c.addEventListener("mouseenter", () =>
+				ui.fcol = { col: x, elem: c });
+			c.addEventListener("mouseleave", () =>
+				ui.fcol = null);
+
+			column.forEach((row, y) => {
+				let b = document.createElement("div");
+					b.classList.add("block");
+					b.innerText = row;
+					c.appendChild(b);
+
+				b.addEventListener("mouseenter", () =>
+					ui.fblock = { col: x, row: y, elem: b });
+				b.addEventListener("mouseleave", () =>
+					ui.fblock = null);
+			});
+		});
+
+	let newColumn = document.createElement("div");
+		newColumn.classList.add("column", "new");
+		ui.content.appendChild(newColumn);
+}
+
+// assign actions
+
 document.getElementById("close").addEventListener("click", () =>
 	window.close());
+
+// initialize
+
+update();
