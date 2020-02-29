448069c12b16b7afbe6742394719bb062d956d2c
not ervin <notminin@pm.me>
2020-02-29 02:09:29 +0000

    style content

--- src/odko.js
+++ src/odko.js
@@ -10,7 +10,7 @@ let ui = {
 
 // stores all of the data of the current project
 let project = {
-	columns: [],
+	columns: [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 2], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1]],
 }
 
 // updates dom of content container to reflect the
@@ -24,10 +24,10 @@ function update() {
 				c.classList.add("column");
 				ui.content.appendChild(c);
 
-			c.addEventListener("mouseenter", () =>
-				ui.fcol = { col: x, elem: c });
-			c.addEventListener("mouseleave", () =>
-				ui.fcol = null);
+			c.addEventListener("mouseenter", () => { if (!ui.flock) {
+					ui.fcol = { col: x, elem: c }; c.classList.add("focus") }});
+			c.addEventListener("mouseleave", () => { if (!ui.flock) {
+					ui.fcol = null; c.classList.remove("focus") }});
 
 			column.forEach((row, y) => {
 				let b = document.createElement("div");
@@ -35,10 +35,10 @@ function update() {
 					b.innerText = row;
 					c.appendChild(b);
 
-				b.addEventListener("mouseenter", () =>
-					ui.fblock = { col: x, row: y, elem: b });
-				b.addEventListener("mouseleave", () =>
-					ui.fblock = null);
+				b.addEventListener("mouseenter", () => { if (!ui.flock) {
+					ui.fblock = { col: x, row: y, elem: b }; b.classList.add("focus") }});
+				b.addEventListener("mouseleave", () => { if (!ui.flock) {
+					ui.fblock = null; b.classList.remove("focus") }});
 			});
 		});
 
